23d6da257f497bd5f74561c156ead43000c2fd9e
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-06-09 15:31:02 +0200

    initial commit

---
+++ src/main/java/com/green/energy/tracker/energy_stream_processor/EnergyStreamProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.green.energy.tracker.energy_stream_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyStreamProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyStreamProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/energy/tracker/energy_stream_processor/EnergyStreamProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.energy.tracker.energy_stream_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyStreamProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
