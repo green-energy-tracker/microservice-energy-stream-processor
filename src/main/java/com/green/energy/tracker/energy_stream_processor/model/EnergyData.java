2baccf478b0c77c1ffd185d37593e1fab428b45f
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-06-10 10:53:05 +0200

    initial commit

--- src/main/java/com/green/energy/tracker/energy_stream_processor/model/EnergyData.java
+++ src/main/java/com/green/energy/tracker/energy_stream_processor/model/EnergyData.java
@@ -1,5 +1,6 @@
 package com.green.energy.tracker.energy_stream_processor.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EnergyData {
+    @JsonProperty("id")
     private Long id;
+    @JsonProperty("sensor_id")
     private Long sensorId;
+    @JsonProperty("timestamp")
     private Long timestamp;
+    @JsonProperty("energy_kwh")
     private String energyKwh;
+    @JsonProperty("quality_flag")
     private Boolean qualityFlag;
 }
