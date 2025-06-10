926829eba581ac0a9237e648705e97e85285ec99
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-06-10 14:46:29 +0200

    initial commit

--- src/main/java/com/green/energy/tracker/energy_stream_processor/webclient/site/SiteWebClient.java
+++ src/main/java/com/green/energy/tracker/energy_stream_processor/webclient/site/SiteWebClient.java
@@ -2,10 +2,11 @@ package com.green.energy.tracker.energy_stream_processor.webclient.site;
 
 import com.green.energy.tracker.configuration.domain.event.Sensor;
 import com.green.energy.tracker.configuration.domain.event.Site;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface SiteWebClient {
     @GetExchange("/findBySensor")
-    Site findBySensor(@RequestParam Sensor sensor);
+    Site findBySensor(@RequestBody Sensor sensor);
 }
