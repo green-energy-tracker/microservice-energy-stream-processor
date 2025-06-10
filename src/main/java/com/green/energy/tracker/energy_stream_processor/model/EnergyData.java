f03542fa70c6efce11a09886c721129119bc88e6
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-06-10 10:40:23 +0200

    initial commit

--- src/main/java/com/green/energy/tracker/energy_stream_processor/model/EnergyData.java
+++ src/main/java/com/green/energy/tracker/energy_stream_processor/model/EnergyData.java
@@ -1,10 +1,14 @@
 package com.green.energy.tracker.energy_stream_processor.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EnergyData {
     private Long id;
     private Long sensorId;
